0c9e7a36a60e4a9fc9e0bbbda7b03d513349e430
unknown <aabdenba@gmail.com>
2023-12-16 10:52:02 -0500

    Stage 1/5 No empty lines here

--- Learning Progress Tracker (Java)/task/src/tracker/Main.java
+++ Learning Progress Tracker (Java)/task/src/tracker/Main.java
@@ -1,7 +1,20 @@
 package tracker;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        System.out.println("Learning Progress Tracker");
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        while(!input.equals("exit")){
+            if(input.isBlank() || input.isEmpty() || input == null){
+                System.out.println("No input.");
+            }else{
+                System.out.println("Error: unknown command!");
+            }
+            input = sc.next();
+        }
+        System.out.println("Bye!");
     }
 }
